80a8161a678f93eb937f4337388e80a43822dc0e
M-Y-Sun <sun.m.yixuan@gmail.com>
2025-02-04 09:40:25 -0800

    add pdf generation and update gitignore

--- build.zig
+++ build.zig
@@ -7,7 +7,6 @@ pub fn build(b: *std.Build) void {
 
     const lib = b.addStaticLibrary(.{
         .name = "cstdio",
-
         .root_source_file = b.path("src/cstdio.zig"),
         .target = target,
         .optimize = optimize,
@@ -36,7 +35,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
+        .root_source_file = b.path("src/cstdio.zig"),
         .target = target,
         .optimize = optimize,
     });
